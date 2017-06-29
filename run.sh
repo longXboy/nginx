cb37678662845f7fcf0c68a6fdadd154354de042
longxboy <longxia.cao@daocloud.io>
2017-06-29 12:20:47 +0800

    fix bug

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-sed "s/DOMAIN/$DOMAIN/g" /default.conf > /etc/nginx/conf.d/default.conf
+sed "s/DOMAIN/$DOMAIN/g" /default.conf | sed "s/EXTERNAL_IP/$EXTERNAL_IP/g" > /etc/nginx/conf.d/default.conf
+
 nginx -g "daemon off;"
